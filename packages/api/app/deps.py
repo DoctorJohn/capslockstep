7a8a480d840cf28e6cd87fd615cb5728e3401982
Jonathan Ehwald <github@ehwald.info>
2026-02-28 00:12:59 +0100

    Add basic api

---
+++ packages/api/app/deps.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import Depends
+from redis.asyncio import Redis
+
+
+def channel_key(room_id: str) -> str:
+    return f"capslockstep:room:{room_id}:channel"
+
+
+def state_key(room_id: str) -> str:
+    return f"capslockstep:room:{room_id}:state"
+
+
+async def get_redis_client():
+    client = Redis.from_url("redis://localhost:6767", decode_responses=True)
+    try:
+        yield client
+    finally:
+        await client.aclose()
+
+
+ChannelKeyDep = Annotated[str, Depends(channel_key)]
+StateKeyDep = Annotated[str, Depends(state_key)]
+RedisClientDep = Annotated[Redis, Depends(get_redis_client)]
---
+++ packages/api/app/main.py
@@ -0,0 +1,54 @@
+import asyncio
+from contextlib import suppress
+from datetime import UTC, datetime
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from app.deps import ChannelKeyDep, RedisClientDep, StateKeyDep
+from app.models import CapsLockEvent, CapsLockState
+
+app = FastAPI()
+
+
+@app.websocket("/caps-lock/{room_id}")
+async def caps_lock_ws(
+    websocket: WebSocket,
+    redis: RedisClientDep,
+    channel_key: ChannelKeyDep,
+    state_key: StateKeyDep,
+):
+    await websocket.accept()
+
+    pubsub = redis.pubsub()
+    await pubsub.subscribe(channel_key)
+
+    async def reader():
+        with suppress(asyncio.CancelledError):
+            async for message in pubsub.listen():
+                if message["type"] == "message":
+                    serialized_state = message["data"]
+                    state = CapsLockState.model_validate_json(serialized_state)
+                    await websocket.send_text(state.model_dump_json())
+
+    reader_task = asyncio.create_task(reader())
+
+    if serialized_state := await redis.get(state_key):
+        state = CapsLockState.model_validate_json(serialized_state)
+        await websocket.send_text(state.model_dump_json())
+
+    try:
+        while True:
+            serialized_state = await websocket.receive_text()
+            event = CapsLockEvent.model_validate_json(serialized_state)
+            state = CapsLockState(value=event.value, date=datetime.now(UTC))
+            serialized_state = state.model_dump_json()
+
+            await redis.set(state_key, serialized_state)
+            await redis.publish(channel_key, serialized_state)
+    except WebSocketDisconnect:
+        pass
+    finally:
+        reader_task.cancel()
+        await reader_task
+        await pubsub.unsubscribe(channel_key)
+        await pubsub.aclose()
